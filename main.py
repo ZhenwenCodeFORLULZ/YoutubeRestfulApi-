debe6bb910c356928f1fdc9410e1b44e94dfee9a
ZhenwenCodeFORLULZ <zhenwende2@gmail.com>
2021-12-15 23:06:55 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse,abort
+from flask_sqlalchemy import SQLAlchemy, Model
+
+
+app = Flask(__name__) # used when you create a new flask app
+api = Api(app)  # wrap our app inside a api
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    # defines all the fields we want in this model
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(100), nullable = False) # nullable = False means that this field needs to have some information
+    views = db.Column(db.Integer, nullable = False)
+    likes = db.Column(db.Integer,nullable = False)
+
+    def __repr__(self):
+        return f"Video(name={name},views = {views},likes={likes})"
+
+#db.create_all() # call it once or else we will overide the columns and rows 
+#db.create_all will make us a database with all these models within it
+
+video_put_args = reqparse.RequestParser()
+#help displays what we send to sender, basically an error message
+video_put_args.add_argument("name", type = str, help = "Name of the Video",required = True)
+# required = True crashes if invalid
+video_put_args.add_argument("views", type = int, help = "Views of the Video",required = True)
+video_put_args.add_argument("likes", type = int, help = "Likes of the Video",required = True)
+# these three are the types of arguements needed for a argument parser
+videos = {}
+
+def abort_if_video_id_dosent_exist(video_id):
+    if video_id not in videos:
+        abort(404,message = "video id is not valid") # abort - sends error message back
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409,message = "Video already exists with that ID...")
+class Video(Resource):
+    def get(self,video_id):
+        abort_if_video_id_dosent_exist(video_id)
+        return videos[video_id]
+
+    def put(self,video_id):
+        abort_if_video_exists(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201 # returns a specific status code 201 - created
+
+    def delete(self,video_id):
+        abort_if_video_id_dosent_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
+#making a new instance of the model
+
+api.add_resource(Video, "/video/<int:video_id>") # <string> allows you to add string info into it
+
+
+if __name__ == "__main__":
+    app.run(debug= True) #debug = True, allows us to see logging info dont run in production
+
+
+
+ 
---
+++ test.py
@@ -0,0 +1,18 @@
+import requests
+BASE = "http://127.0.0.1:5000/" #location of the API and the server its running on
+data = [
+    {"likes":78,"name": "Joe","views":1000000},
+    {"likes":100000,"name": "How to make Rest API","views":8000000},
+    {"likes":35,"name": "Zhen","views":2000}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+#response = requests.put(BASE + "video/1",{"likes":10,"name": " Tim","views":1000000})
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
